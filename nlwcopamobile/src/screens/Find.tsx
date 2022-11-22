5a3907c259e1c4703515d97ad4ed347ebafc3cb3
Danubio de Araújo <danubio.bwm@gmail.com>
2022-11-21 22:17:17 -0300

    Serve Finishe

---
+++ nlwcopamobile/src/screens/Find.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { VStack, Heading } from "native-base";
+
+import { Header } from "../components/Header";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
+export function Find() {
+  return (
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="BUSCAR POR CÓDIGO" showBackButton/>
+      <VStack mt={8} mx={5} alignItems="center">
+        <Heading
+          fontFamily="heading"
+          color="white"
+          fontSize="xl"
+          mb={8}
+          textAlign="center"
+        >
+          Encontre um bolão através de seu código único
+        </Heading>
+        <Input mb={2} placeholder="Qual o código do bolão?" />
+        <Button mt={2} title="BUSCAR BOLÃO" />
+      </VStack>
+    </VStack>
+  );
+}
